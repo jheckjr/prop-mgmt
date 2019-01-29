e997b210c8ee477dcd1d236cebb5488c68a87034
jheckjr <jheckjr@users.noreply.github.com>
2019-01-29 04:48:06 +0000

    Add pages

---
+++ src/pages/home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.css';
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div className="home">
+        <div className="lander">
+          <h1>Property Management</h1>
+          <p>A frustration-free property management system</p>
+        </div>
+      </div>
+    );
+  }
+}
